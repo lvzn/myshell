f9c784135764b8fa2b5d12212f6d807ec69b0c16
Leevi <joona.mlin@gmail.com>
2022-03-24 15:19:53 +0200

    parsing works

--- main.c
+++ main.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define PATH "/bin"
+
+void execute(char** command) {
+    //todo
+}
+
+void parseCommand(char* line) {
+    int i = 0;
+    char* token = NULL;
+    char** command = (char**)malloc(sizeof(char));
+    while ((token = strtok_r(line, " ", &line)) != NULL) {
+        command[i] = strdup(token);
+        i++;
+    }
+    execute(command);
+}
 
 void listen() {
     size_t length = 0;
     char* line = NULL;
     printf("wish> ");
     while (getline(&line, &length, stdin) != -1) {
-        puts("works");
+        parseCommand(line);
         printf("wish> ");
     }
     free(line);
 }
 
 int main(int argc, char** argv) {
-    while (1) {
+    if (argc == 2) {
+        //todo batch mode
+    } else {
         listen();
     }
-
 }
