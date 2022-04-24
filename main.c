0e37bb1b307ed5d122719ec6f6aacfe2ee76dc8c
Leevi <joona.mlin@gmail.com>
2022-04-24 20:46:05 +0300

    added comments, readme

--- main.c
+++ main.c
@@ -7,21 +7,21 @@
 
 //Param 1: /bin/ls/
 
-//todo add /usr/bin/ to PATH
 
 int execute(char** command) {
     int rc_wait;
+    //fork the shell, creating a subprocess for the command that will be run
     int rc = fork();
     //Dynamically allocation memory for the path
     char* PATH = (char*)malloc(sizeof(char*) * 5 + sizeof(char*) * strlen(command[0]));
     strcpy(PATH, "/bin/");
     strcat(PATH, command[0]);
-    //strcat(dir, command[0]);
     if (rc < 0) {
+        //the case where the fork fails
         fprintf(stderr, "Fork failed.\n");
     } else if (rc == 0) {
-        //child
-        //Checking executing rights for the command
+        //the path the child process will head to
+        //Checking whether the command is able to be executed
         if (access(PATH, X_OK) == 0) {
             execv(PATH, command);
         } else {
@@ -34,9 +34,10 @@ int execute(char** command) {
             }
         }
     } else {
+        //the path the parent (shell) heads to, during the fork
         rc_wait = wait(NULL);
     }
-    free(PATH);
+    free(PATH); //free the memory allocated at the beginning
     return 1;
 }
 
@@ -65,10 +66,10 @@ void listen() {
     int status = 1;
     size_t length = 0;
     char* line = NULL;
-    //Printing the indicator that the shell works
+    //Printing the shell prompt
     printf("wish> ");
     while (getline(&line, &length, stdin) != -1) {
-        //Excluding "Enter" from the command
+        //Excluding the newline character from the line
         line[strlen(line) - 1] = '\0';
         status = parseCommand(line);
         if (status == 0)
@@ -81,10 +82,6 @@ void listen() {
 }
 
 int main(int argc, char** argv) {
-    if (argc == 2) {
-        //todo batch mode
-    } else {
-        listen();
-    }
+    listen();
     return 0;
 }
